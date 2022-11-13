ec00bfec605f7a93466e0ba9f3fc17b5352ec3f9
Okky Pratama <100081121+okkyPratama@users.noreply.github.com>
2022-11-13 22:18:06 +0700

    Add random quote generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [quoteInfo, setQuoteInfo] = useState({});
+
+  useEffect(() => {
+
+  }, [])
+
+  const getQuote = () => {
+    fetch(`https://api.quotable.io/random`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      setQuoteInfo({
+        text: data.content,
+        author: data.author,
+      });
+     
+    })}
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+
+
+      <div id='quote-box' className=' card rounded  mt-5' >
+        <p id='text' className='pt-3'>{quoteInfo.text}</p>
+        <p id='author'> -  {quoteInfo.author} </p>
+
+      <div className='btn-container'>
+        <div className='item'>
+        <a href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + quoteInfo.text + ' ' + '-'+quoteInfo.author} target="_blank" id='tweet-quote' className='btn btn-info'>
+        <i class="bi bi-twitter"></i>
         </a>
-      </header>
+        </div>
+
+        <div className='item'>
+        <button id='new-quote' onClick={getQuote} className='btn btn-dark'>
+          New Quote
+        </button>
+     
+        </div>
+
+      </div>
+
+
+      </div>
+
     </div>
-  );
+  )
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import $ from 'jquery';
+import Popper from 'popper.js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
